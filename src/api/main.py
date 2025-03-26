d69b3e2bcfd0ae943b44028c4c8214289773eb07
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-03-26 08:40:21 +0100

    fix: remove caching implementation

--- src/api/main.py
+++ src/api/main.py
@@ -3,13 +3,7 @@ from .schemas import TextInput, SentimentOutput
 
 from transformers import pipeline
 
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from fastapi_cache.decorator import cache
-
-from redis import asyncio as aioredis
 from functools import lru_cache
-from contextlib import asynccontextmanager
 
 
 @lru_cache(maxsize=1)
@@ -17,21 +11,13 @@ def get_sentiment_analyzer():
     """Initialize and cache the sentiment analysis pipeline"""
     return pipeline("sentiment-analysis")
 
-@asynccontextmanager
-async def startup(_: FastAPI):
-    redis = aioredis.from_url("redis://localhost")
-    FastAPICache.init(RedisBackend(redis), prefix="sentiment-cache")
-    yield
-
-app = FastAPI(lifespan=startup)
-
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "This is an example API"}
 
 @app.post("/predict-sentiment")
-# @cache(expire=3600)
 async def predict_sentiment(input: TextInput) -> SentimentOutput:
     try: 
         sentiment_analyzer = get_sentiment_analyzer()
